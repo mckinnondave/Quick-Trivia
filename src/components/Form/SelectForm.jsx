94fe9456366d212cc8c11fe64344bed0807350ed
Dave <dave.w.mckinnon@gmail.com>
2022-06-08 23:27:23 -0700

    add dropdown

--- src/components/Form/SelectForm.jsx
+++ src/components/Form/SelectForm.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './SelectForm.scss'
 import getInfo from '../../helpers/getInfo'
+import Select from 'react-select';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
 
 export default function SelectForm () {
+  const [selectedOption, setSelectedOption] = useState(null);
+  console.log(selectedOption);
   
   return (
     <div className="select-form">
-      <form>
-        
-      </form>
+      <Select
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+        options={options}
+      />
     </div>
   )
 }
