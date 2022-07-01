0fde324843ab9141523f8bfb83f6e377aa3891ab
Dave <dave.w.mckinnon@gmail.com>
2022-06-30 17:15:54 -0700

    fix files

--- server/index.js
+++ server/index.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const ScoreModel = require("./models/Scores");
 
-mongoose.connect("mongodb+srv://mckinnondave:Vaughnhockey2@triviacluster.eyyonie.mongodb.net/trivia-app?retryWrites=true&w=majority")
+const cors = require('cors')
+
+app.use(express.json());
+app.use(cors())
+
+mongoose.connect(
+  "mongodb+srv://mckinnondave:Vaughnhockey2@triviacluster.eyyonie.mongodb.net/trivia-app?retryWrites=true&w=majority"
+);
+
+app.get("/getScores", (req, res) => {
+  ScoreModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/postScore", async (req, res) => {
+  const score = req.body
+  const newScore = new ScoreModel(score);
+  await newScore.save();
+
+  res.json(score)
+})
 
 app.listen(3001, () => {
   console.log("SERVER IS RUNNING!");
-})
+});
--- server/models/Scores.js
+++ server/models/Scores.js
@@ -1,5 +1,19 @@
 const mongoose = require("mongoose");
 
 const ScoreSchema = new mongoose.Schema({
-  
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+});
+
+const ScoreModel = mongoose.model("scores", ScoreSchema)
+module.exports = ScoreModel;
