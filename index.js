3356785463f154899a827e6add8efdc9f42b0d6d
Dave <dave.w.mckinnon@gmail.com>
2022-07-05 17:53:27 -0700

    first commit for heroku

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const ScoreModel = require("./models/Scores");
+require('dotenv').config()
+
+const cors = require('cors')
+
+app.use(express.json());
+app.use(cors())
+
+mongoose.connect(
+  `${process.env.MONGODB_URI}`
+);
+
+app.get("/getScores", (req, res) => {
+  ScoreModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/postScore", async (req, res) => {
+  const score = req.body
+  const newScore = new ScoreModel(score);
+  await newScore.save();
+
+  res.json(score)
+})
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log("SERVER IS RUNNING!");
+});
